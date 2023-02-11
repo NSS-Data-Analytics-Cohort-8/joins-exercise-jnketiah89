acfabd79d026a456a5db846dfe816f2b1da7a9ba
Jasmine Nketiah <jasmine.nketiah@gmail.com>
2023-02-11 10:54:31 -0600

    working on Q3

---
+++ scripts/joins.sql
@@ -0,0 +1,19 @@
+1. Give the name, release year, and worldwide gross of the lowest grossing movie.
+
+--SELECT film_title.specs, release_year.specs
+FROM specs
+INNER JOIN ON worldwide_gross.revenue;
+
+
+2. What year has the highest average imdb rating?
+
+3. What is the highest grossing G-rated movie? Which company distributed it?
+
+4. Write a query that returns, for each distributor in the distributors table, the distributor name and the number of movies associated with that distributor in the movies 
+table. Your result set should include all of the distributors, whether or not they have any movies in the movies table.
+
+5. Write a query that returns the five distributors with the highest average movie budget.
+
+6. How many movies in the dataset are distributed by a company which is not headquartered in California? Which of these movies has the highest imdb rating?
+
+7. Which have a higher average rating, movies which are over two hours long or movies which are under two hours
